704d027cc333b2ec9fec8b9cbbd014c6565adfed
njavor <nikolettjavor@gmail.com>
2026-05-19 14:38:14 +0200

    OTK-AAI2EG-INF22 | intervallumhalmaz

---
+++ semesters/25-26-2/OTK-AAI2EG-INF22/kod/halmaz.cs
@@ -0,0 +1,110 @@
+class Halmaz
+{
+    int MaxDb = 100;
+    int db;
+    int[] elemek;
+
+    public Halmaz()
+    {
+        db = 0;
+        elemek = new int[MaxDb];
+    }
+
+    public void Beolvasas()
+    {
+        db = int.Parse(Console.ReadLine());
+        for (int i = 0; i < db; i++)
+        {
+            elemek[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    public void Kiiras()
+    {
+        Console.WriteLine(db);
+        for (int i = 0; i < db; i++)
+        {
+            Console.Write(elemek[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    public void Halmazba(int elem)
+    {
+        if (!ElemeE(elem))
+        {
+            elemek[db] = elem;
+            db++;
+        }
+    }
+    public void Halmazbol(int elem)
+    {
+        int i = 0;
+        while (i < db && elemek[i] != elem) i++;
+        if (i < db)
+        {
+            elemek[i] = elemek[db - 1];
+            db--;
+        }
+    }
+    public void Urites()
+    {
+        db = 0;
+    }
+    
+
+    public bool ElemeE(int elem)
+    {
+        int i = 0;
+        while (i < db && elemek[i] != elem) i++;
+        return i < db;
+    }
+    public bool UresE()
+    {
+        return db == 0;
+    }
+    public bool ReszeE(Halmaz masik)
+    {
+        if (masik.db < this.db) return false;
+
+        int i = 0;
+        while (i < db && masik.ElemeE(elemek[i])) i++;
+        return i >= db;
+    }
+    public bool EgyenloE(Halmaz masik)
+    {
+        if (db != masik.db) return false;
+        for (int i = 0; i < db; i++)
+        {
+            if (!masik.ElemeE(elemek[i])) return false;
+        }
+        return true;
+    }
+    
+
+    public Halmaz Unio(Halmaz masik)
+    {
+        Halmaz unio = new Halmaz();
+        for (int i = 0; i < this.db; i++) unio.Halmazba(this.elemek[i]);
+        for (int i = 0; i < masik.db; i++) unio.Halmazba(masik.elemek[i]);
+        return unio;
+    }
+    public Halmaz Metszet(Halmaz masik)
+    {
+        Halmaz metszet = new Halmaz();
+        for (int i = 0; i < this.db; i++)
+        {
+            if (masik.ElemeE(elemek[i])) metszet.Halmazba(elemek[i]);
+        }
+        return metszet;
+    }
+    public Halmaz Kulonbseg(Halmaz masik)
+    {
+        Halmaz kulonbseg = new Halmaz();
+        for (int i = 0; i < this.db; i++)
+        {
+            if (!masik.ElemeE(elemek[i])) kulonbseg.Halmazba(elemek[i]);
+        }
+        return kulonbseg;
+    }
+}
---
+++ semesters/25-26-2/OTK-AAI2EG-INF22/kod/intervallumhalmaz.cs
@@ -0,0 +1,204 @@
+class IntervallumHalmaz
+{
+    int MaxDb = 100;
+    int db;
+    int[] kezd;
+    int[] veg;
+
+    public IntervallumHalmaz()
+    {
+        db = 0;
+        kezd = new int[MaxDb];
+        veg = new int[MaxDb];
+    }
+    public IntervallumHalmaz(int db, int[] kezd, int[] veg)
+    {
+        this.db = db;
+        this.kezd = kezd;
+        this.veg = veg;
+    }
+    
+    
+    public void Beolvasas()
+    {
+        db = int.Parse(Console.ReadLine());
+        for (int i = 0; i < db; i++)
+        {
+            string[] sor = Console.ReadLine().Split();
+            kezd[i] = int.Parse(sor[0]);
+            veg[i] = int.Parse(sor[1]);
+        }
+    }
+    public void Kiiras()
+    {
+        Console.WriteLine(db);
+        for (int i = 0; i < db; i++)
+        {
+            Console.WriteLine(kezd[i] + " " + veg[i]);
+        }
+    }
+    
+
+    public void Intervallumhalmazba(int kezd, int veg)
+    {
+        //tudú
+    }
+    public void Multihalmazbol(int kezd, int veg)
+    {
+        // tudú
+    }
+    public void Urites()
+    {
+        db = 0;
+    }
+    #endregion
+
+    #region Logikai vizsgalatok
+    public bool ElemeE(int ertek)
+    {
+        int i = 0;
+        while (i < db && (ertek < kezd[i] || ertek > veg[i])) i++;
+        return i < db;
+    }
+    public bool UresE()
+    {
+        return db == 0;
+    }
+    
+
+    public IntervallumHalmaz Unio(IntervallumHalmaz masik)
+    {
+        if (db == 0) return masik;
+        if (masik.db == 0) return this;
+        IntervallumHalmaz unio = new IntervallumHalmaz();
+        int udb = 0;
+        int i = 0;
+        int j = 0;
+
+        int akt_kezd;
+        int akt_veg;
+
+        if (kezd[i] < masik.kezd[j])
+        {
+            akt_kezd = kezd[i];
+            akt_veg = veg[i];
+            i++;
+        }
+        else
+        {
+            akt_kezd = masik.kezd[j];
+            akt_veg = masik.veg[j];
+            j++;
+        }
+
+        while (i < db && j < masik.db)
+        {
+            if (kezd[i] < masik.kezd[j])
+            {
+                if (akt_veg < kezd[i])
+                {
+                    unio.kezd[udb] = akt_kezd;
+                    unio.veg[udb] = akt_veg;
+                    udb++;
+
+                    akt_kezd = kezd[i];
+                    akt_veg = veg[i];
+                    i++;
+                }
+                else
+                {
+                    akt_veg = Math.Max(akt_veg, veg[i]);
+                    i++;
+                }
+            }
+            else
+            {
+                if (akt_veg < masik.kezd[j])
+                {
+                    unio.kezd[udb] = akt_kezd;
+                    unio.veg[udb] = akt_veg;
+                    udb++;
+
+                    akt_kezd = masik.kezd[j];
+                    akt_veg = masik.veg[j];
+                    j++;
+                }
+                else
+                {
+                    akt_veg = Math.Max(akt_veg, masik.veg[j]);
+                    j++;
+                }
+            }
+        }
+        while (i < db)
+        {
+            if (akt_veg < kezd[i])
+            {
+                unio.kezd[udb] = akt_kezd;
+                unio.veg[udb] = akt_veg;
+                udb++;
+
+                akt_kezd = kezd[i];
+                akt_veg = veg[i];
+                i++;
+            }
+            else
+            {
+                akt_veg = Math.Max(akt_veg, veg[i]);
+                i++;
+            }
+        }
+        while (j < masik.db)
+        {
+            if (akt_veg < masik.kezd[j])
+            {
+                unio.kezd[udb] = akt_kezd;
+                unio.veg[udb] = akt_veg;
+                udb++;
+
+                akt_kezd = masik.kezd[j];
+                akt_veg = masik.veg[j];
+                j++;
+            }
+            else
+            {
+                akt_veg = Math.Max(akt_veg, masik.veg[j]);
+                j++;
+            }
+        }
+
+        unio.kezd[udb] = akt_kezd;
+        unio.veg[udb] = akt_veg;
+        udb++;
+
+        unio.db = udb;
+        return unio;
+    }
+    public IntervallumHalmaz Metszet(IntervallumHalmaz masik)
+    {
+        IntervallumHalmaz metszet = new IntervallumHalmaz();
+        int mdb = 0;
+        int i = 0;
+        int j = 0;
+        while (i < db && j < masik.db)
+        {
+            if (veg[i] < masik.kezd[j]) { i++; }
+            else if (kezd[i] > masik.veg[j]) { j++; }
+            else
+            {
+                metszet.kezd[mdb] = Math.Max(kezd[i], masik.kezd[j]);
+                metszet.veg[mdb] = Math.Min(veg[i], masik.veg[j]);
+                mdb++;
+                if (veg[i] < masik.veg[j]) { i++; }
+                else if (masik.veg[j] < veg[i]) { j++; }
+                else
+                {
+                    i++;
+                    j++;
+                }
+            }
+        }
+        metszet.db = mdb;
+        return metszet;
+    }
+}
